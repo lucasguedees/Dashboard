2d6ed6eb86eaf49d213655ea64e94d9fa5615c1c
lucasguedees <lucasm.guedes@yahoo.com.br>
2026-01-16 19:12:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  base: "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, ""),
-    },
-  },
 });
